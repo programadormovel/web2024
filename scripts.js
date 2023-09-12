7483104005ec60e1f24e32711d4e6ca3786429d2
programadormovel <programadormovel@gmail.com>
2023-09-12 10:46:55 -0300

    estudo bootstrap jquery

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("p:first").click(function () {
+        $(this).hide();
+    });
+
+    $("h1").click(function () {
+        alert("Bora aprender C#!");
+    });
+});
+
+$(document).ready(function(){
+    $("#windows").click(function(){
+        $("#p1").fadeIn();
+        $("#p2").fadeIn("slow");
+        $("#p3").fadeIn(3000);
+      });
+  });
+
+  
+$(document).ready(function(){
+      $("#web").click(function(){
+        $("#p4").fadeIn();
+        $("#p5").fadeIn("slow");
+        $("#p6").fadeIn(3000);
+      });
+  });
+
+  
+$(document).ready(function(){
+      $("#mobile").click(function(){
+        $("#p7").fadeIn();
+        $("#p8").fadeIn("slow");
+        $("#p9").fadeIn(3000);
+      });
+  });
